874e65e93ba0687f4d95100143b5973035fe1086
msakamoto-michiaki <msakamoto.michiaki@gmail.com>
2026-01-12 13:04:37 +0900

    update

---
+++ git_init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# ① ディレクトリに移動
+cd /Users/sakamotomichiaki/work/git/ips_compensation
+
+# ② Gitリポジトリとして初期化（すでに行っている場合はスキップ可）
+git init
+
+# ③ すべてのファイルをステージングに追加
+git add .
+
+# ④ 最初のコミット
+git commit -m "initial commit"
+
+# ⑤ リモートリポジトリ（GitHub）を登録
+# ※ [コピーしたURL] の部分は先ほどGitHubでコピーしたものに書き換えてください
+git remote add origin https://github.com/msakamoto-michiaki/ips_compensation.git
+
+# ⑥ GitHubへ送信（Push）
+git branch -M main
+git push -u origin main
